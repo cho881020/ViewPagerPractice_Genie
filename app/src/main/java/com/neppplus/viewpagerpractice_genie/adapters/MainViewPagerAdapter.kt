d7fe6cd53a6cdc514e0bbdcdbe411d575e1229a2
Geine <cho881020@gmail.com>
2021-05-08 15:20:50 +0900

    페이지별 제목 설정

--- app/src/main/java/com/neppplus/viewpagerpractice_genie/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/neppplus/viewpagerpractice_genie/adapters/MainViewPagerAdapter.kt
@@ -9,6 +9,18 @@ import com.neppplus.viewpagerpractice_genie.fragments.NameFragment
 
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
 
+    override fun getPageTitle(position: Int): CharSequence? {
+        if (position == 0) {
+            return "이름"
+        }
+        else if (position == 1) {
+            return "내 정보"
+        }
+        else {
+            return "인사말"
+        }
+    }
+    
     override fun getItem(position: Int): Fragment {
 
         if (position == 0) {
