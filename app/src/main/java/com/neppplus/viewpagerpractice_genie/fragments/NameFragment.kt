64f3d14405bd2052788ad6b498086f9cefa37783
Geine <cho881020@gmail.com>
2021-05-08 14:50:15 +0900

    뷰페이저에 배치될 3개의 프래그먼트 완성

---
+++ app/src/main/java/com/neppplus/viewpagerpractice_genie/fragments/NameFragment.kt
@@ -0,0 +1,26 @@
+package com.neppplus.viewpagerpractice_genie.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.viewpagerpractice_genie.R
+
+class NameFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_name, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+    }
+
+}
